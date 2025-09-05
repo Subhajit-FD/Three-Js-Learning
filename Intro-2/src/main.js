e09099a18b1f11f5593e73b5b05747856548a817
Subhajit Choudhury <122729012+Subhajit-FD@users.noreply.github.com>
2025-09-05 21:38:32 +0530

    Texture basic

--- Intro-2/src/main.js
+++ Intro-2/src/main.js
@@ -3,9 +3,15 @@ import * as THREE from "three";
 //SCENE
 const scene = new THREE.Scene();
 
+//TEXTURE
+const textureLoader = new THREE.TextureLoader();
+const grass = textureLoader.load("/texture.jpg");
+grass.colorSpace = THREE.SRGBColorSpace;
+grass.minFilter = THREE.NearestFilter
+
 //BOX OBJECT
-const geometry = new THREE.BoxGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: "Blue" });
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ map: grass });
 
 const box = new THREE.Mesh(geometry, material);
 
@@ -22,15 +28,28 @@ const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 camera.position.z = 4;
 scene.add(camera);
 
+//RESIZING SCREEN
+window.addEventListener("resize", () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(size.width, size.height);
+});
+
 //RENDERER
 const canvas = document.querySelector("#canvas");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 //ANIMATION
 
+const clock = new THREE.Clock();
 const tick = () => {
-  box.rotation.y += 0.01
+  const elapsedTime = clock.getElapsedTime();
+  box.rotation.y = elapsedTime;
+  box.position.y = Math.sin(elapsedTime);
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
